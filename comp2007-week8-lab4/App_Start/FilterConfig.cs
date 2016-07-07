8422d3c2f027ed0f3d356ca4bf62e937aadd2479
p1brighto <p1brighto@gmail.com>
2016-07-07 17:33:51 -0400

    Add project files.

---
+++ comp2007-week8-lab4/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace comp2007_week8_lab4
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ comp2007-week8-lab4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="comp2007_week8_lab4.MvcApplication" Language="C#" %>
